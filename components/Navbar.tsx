a952f704ad5fc569c2506b4958ebf4f951e883bc
Zahra-Shadman <zahra.shadmani.work@gmail.com>
2025-05-15 15:51:40 +0330

    add nav and home components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,37 @@
-
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-const Navbar: React.FC = () => {
-  const [hover, isHover] = useState(false);
-  return (
-    <div className="flex justify-end left-[188px] gap-10 relative w-[1064px] text-gray-200">
 
-        <div className="mt-10 flex gap-10">
-      <Link href={"/hireMe"}>همکاری با من</Link>
-      <Link href={"/projects"}>پروژه ها</Link>
-      <Link href={"/expriences"}>سوابق</Link>
-      <Link href={"/aboutMe"}>درباره من</Link>
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex justify-end gap-10 relative w-full max-w-[1064px] text-gray-200 px-4 font-medium">
+      <div className="mt-10 flex flex-wrap gap-10">
+        <Link
+          href={"/hireMe"}
+          className="hover:text-blue-300 transition duration-200"
+        >Hire Me
+        </Link>
+        <Link
+          href={"/projects"}
+          className="hover:text-blue-300 transition duration-200"
+        >
+          Projects
+        </Link>
+        <Link
+          href={"/experiences"}
+          className="hover:text-blue-300 transition duration-200"
+        >
+          Experiences
+        </Link>
+        <Link
+          href={"/aboutMe"}
+          className="hover:text-blue-300 transition duration-200"
+        >
+          About Me
+        </Link>
       </div>
     </div>
   );
 };
+
 export default Navbar;
--- components/home.tsx
+++ components/home.tsx
@@ -1,10 +1,16 @@
+"use client";
+import HeroSection from "./HeroSection";
 import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-[#04152D] w-full min-h-screen relative">
+    <div className="bg-[#04152D] w-full min-h-screen opacity-100 relative">
       <Navbar />
-      <div className="bg-[#19376DB2] blur-[200px] animate-[wiggle_1s_ease-in-out_infinite] w-[760px] h-[674px] rounded-full opacity-70 absolute top-[-128px] left-[-216px]"></div>
+      <div className="bg-[#19376DB2]  blur-[200px]  w-[60vw] h-[60vw] max-w-[760px]
+       max-h-[674px] rounded-full opacity-90 absolute top-[-20%] left-[-30%] md:top-[-128px] md:left-[-216px]"></div>
+      <div className="bg-[#19376DB2] blur-[200px] animate-pulse w-[50vw] h-[50vw] max-w-[600px] max-h-[450px]
+       rounded-full opacity-90 absolute top-[20%] left-[70%] md:top-[100px] md:left-[800px]"></div>
+      <HeroSection />
     </div>
   );
 }
